50af8d427ccfb43d2ddf15557dbe711092875e25
JacoboOrozco <57960290+JacoboOrozco@users.noreply.github.com>
2021-09-06 22:21:01 +0000

    WORKING SEPARATION
    This is the first functional implementation of a function separated algorithm that gives the product of the nxn matrices.

--- matrix.cpp
+++ matrix.cpp
@@ -4,42 +4,39 @@
 using namespace std;
 
 
-int main()
+void printMatrix(int n,int** matrix)
 {
-    std::cout << "Welcome To The Matrix Product Software!\n";
-    srand(time(NULL));
-    cout << "Please input the size of the nxn matrix";
-    int input;
-    cin >> input;
-    const int n = input;
-    int matrix1[n][n];
-    int matrix2[n][n];
-    int result[n][n];
-    cout << "First Matrix: " << endl;
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            matrix1[i][j] = rand() % 50;
-            cout << matrix1[i][j] << "\t";
+            cout << matrix[i][j] << "\t";
         }
         cout << endl;
     }
+}
 
-    cout << "Second Matrix: " << endl;
+int** createMatrix(int n)
+{
+    int** matrix = new int*[n];
     for (int i = 0; i < n; i++)
     {
+        matrix[i] = new int[n];
         for (int j = 0; j < n; j++)
         {
-            matrix2[i][j] = rand() % 50;
-            cout << matrix2[i][j] << "\t";
+            matrix[i][j] = rand() % 50;
         }
-        cout << endl;
-    }
+    } 
+    return matrix;
+}
 
 
+int** calculateProduct(int n, int** matrix1, int** matrix2)
+{
+    int** result = new int*[n];
     for (int resRow = 0; resRow < n; resRow++)
     {
+        result[resRow] = new int[n];
         for (int resCol = 0; resCol < n; resCol++)
         {
             result[resRow][resCol] = 0;
@@ -48,20 +45,35 @@ int main()
         }
     }
 
-
-    cout << "Result matrix is \n";
-    for (int row = 0; row < n; row++)
-    {
-        for (int col = 0; col < n; col++)
-        {
-            cout << result[row][col] << "\t";
-        }
-        cout << "\n";
-    }
+    return result;
+}
 
 
+int main()
+{
+    std::cout << "Welcome To The Matrix Product Software!\n";
+    srand(time(NULL));
+    cout << "Please input the size of the nxn matrix";
+    int input;
+    cin >> input;
+    int const n = input;
+    int **matrix1;
+    int **matrix2;
+    int **result;
+    matrix1 = createMatrix(n);
+    cout << endl << "Matrix 1: " << endl;
+    printMatrix(n, matrix1);
+
+    
+    matrix2 = createMatrix(n);
+    cout << endl << "Matrix 2: " << endl;
+    printMatrix(n, matrix2);
+
+
+    result = calculateProduct(n, matrix1, matrix2);
+    cout << endl << "Resulting Matrix 2" << endl;
+    printMatrix(n, result);
+    
     system("PAUSE");
-
     return 0;
-
 }
